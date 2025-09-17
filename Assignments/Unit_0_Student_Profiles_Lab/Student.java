f41955e30f085cab787802a41e7d959700b39d49
Justin Yu <120344386+zaizai716@users.noreply.github.com>
2025-09-17 14:56:58 -0700

    Student Lab

---
+++ Assignments/Unit_0_Student_Profiles_Lab/Student.java
@@ -0,0 +1,59 @@
+public class Student {
+
+    private String name = "Justin";
+    private String id = "XXX-XXXX";
+    private int grade = 0;
+
+    // constructors
+    public Student(String Pname, int Pgrade) {
+        this.name = Pname;
+        this.grade = Pgrade;
+        this.id = generateId();
+    }
+
+    public Student(String Pname) {
+        this.name = Pname;
+        this.grade = 10;
+    }
+
+    // getters
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    // setters
+    public void setName(String input) {
+        name = input;
+    }
+
+    public void setId(String input) {
+        id = input;
+    }
+
+    public void setGrade(int input) {
+        grade = input;
+    }
+
+    // methods
+    public String toString() {
+        return (name + " is a " + grade + "th grader. Their id is " + id + ".");
+    }
+
+    public boolean equals(Student other) {
+        return (name == other.name && id == other.id && grade == other.grade);
+    }
+
+    public String generateId() {
+        return ((Math.random() * 9) + 1)
+    }
+
+
+}
