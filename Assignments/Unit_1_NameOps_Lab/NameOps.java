6a550aae3a8dd276d1668b6c37f9deef867ad1ab
Justin Yu <120344386+zaizai716@users.noreply.github.com>
2025-10-22 12:57:18 -0700

    commit and push

--- Assignments/Unit_1_NameOps_Lab/NameOps.java
+++ Assignments/Unit_1_NameOps_Lab/NameOps.java
@@ -1,15 +1,112 @@
+/**
+ * A utility class for performing various operations on names.
+ * Provides methods to extract first names, last names, middle names,
+ * and format names in different ways.
+ */
 public class NameOps {
-    // methods
+    /**
+     * Creates a formatted string for method call display.
+     *
+     * @param method the method name
+     * @param name the name parameter
+     * @return formatted string showing method call
+     */
     public static String printMethodCall(String method, String name) {
-        return method + "(\"" + name + "\"):";
+        return method + "(\"" + name + "\"): ";
     }
 
+    /**
+     * Returns a string indicating that the given name is awesome.
+     *
+     * @param name the name to make awesome
+     * @return formatted string with "is awesome!"
+     */
     public static String whoIsAwesome(String name) {
         return name + " is awesome!";
     }
 
+    /**
+     * Finds the index of the first space in the given name.
+     *
+     * @param name the name to search
+     * @return index of first space, or -1 if no space found
+     */
     public static int indexOfFirstSpace(String name) {
         return name.indexOf(" ");
     }
 
- }
+    /**
+     * Finds the index of the second space in the given name.
+     *
+     * @param name the name to search
+     * @return index of second space, or -1 if no second space found
+     */
+    public static int indexOfSecondSpace(String name) {
+        int firstSpace = name.indexOf(" ");
+        return name.indexOf(" ", firstSpace + 1);
+    }
+
+    /**
+     * Extracts the first name from a full name.
+     *
+     * @param name the full name
+     * @return the first name, or the entire name if no space found
+     */
+    public static String findFirstName(String name) {
+        int firstSpace = name.indexOf(" ");
+        if (firstSpace == -1) {
+            return name; // No space found, return the whole name
+        }
+        return name.substring(0, firstSpace);
+    }
+
+    /**
+     * Extracts the last name from a full name.
+     *
+     * @param name the full name
+     * @return the last name, or empty string if no space found
+     */
+    public static String findLastName(String name) {
+        int lastSpace = name.lastIndexOf(" ");
+        if (lastSpace == -1) {
+            return ""; // No space found, return empty string
+        }
+        return name.substring(lastSpace + 1);
+    }
+
+    /**
+     * Extracts the middle name from a full name.
+     *
+     * @param name the full name
+     * @return the middle name, or empty string if no middle name found
+     */
+    public static String findMiddleName(String name) {
+        int firstSpace = name.indexOf(" ");
+        int lastSpace = name.lastIndexOf(" ");
+        if (firstSpace == -1 || lastSpace == -1 || firstSpace == lastSpace) {
+            return ""; // No middle name found
+        }
+        return name.substring(firstSpace + 1, lastSpace);
+    }
+
+    /**
+     * Generates a formatted name string in "Last, First M." format.
+     *
+     * @param name the full name to format
+     * @return formatted name string
+     */
+    public static String generateLastFirstMidInitial(String name) {
+        String lastName = findLastName(name);
+        String middleName = findMiddleName(name);
+        String firstName = findFirstName(name);
+        
+        if (lastName.isEmpty()) {
+            return firstName; // Only one name
+        } else if (middleName.isEmpty()) {
+            return lastName + ", " + firstName; // Two names
+        } else {
+            char firstChar = middleName.charAt(0);
+            return lastName + ", " + firstName + " " + firstChar + "."; // Three names
+        }
+    }
+}
--- Assignments/Unit_1_NameOps_Lab/NameOpsTester.java
+++ Assignments/Unit_1_NameOps_Lab/NameOpsTester.java
@@ -16,70 +16,70 @@ public class NameOpsTester {
         System.out.println();
 
         // // testing indexOfFirstSpace()
-        // System.out.println("== Testing indexOfFirstSpace() ==");
-        // method = "indexOfFirstSpace";
-        // test1 = NameOps.printMethodCall(method, name1) + NameOps.indexOfFirstSpace(name1);
-        // test2 = NameOps.printMethodCall(method, name2) + NameOps.indexOfFirstSpace(name2);
-        // test3 = NameOps.printMethodCall(method, name3) + NameOps.indexOfFirstSpace(name3);
-        // System.out.println(test1); // -1
-        // System.out.println(test2); // 5
-        // System.out.println(test3); // 5
-        // System.out.println();
+        System.out.println("== Testing indexOfFirstSpace() ==");
+        method = "indexOfFirstSpace";
+        test1 = NameOps.printMethodCall(method, name1) + NameOps.indexOfFirstSpace(name1);
+        test2 = NameOps.printMethodCall(method, name2) + NameOps.indexOfFirstSpace(name2);
+        test3 = NameOps.printMethodCall(method, name3) + NameOps.indexOfFirstSpace(name3);
+        System.out.println(test1); // -1
+        System.out.println(test2); // 5
+        System.out.println(test3); // 5
+        System.out.println();
 
-        // // testing indexOfSecondSpace()
-        // System.out.println("== Testing indexOfSecondSpace() ==");
-        // method = "indexOfSecondSpace";
-        // test1 = NameOps.printMethodCall(method, name1) + NameOps.indexOfSecondSpace(name1);
-        // test2 = NameOps.printMethodCall(method, name2) + NameOps.indexOfSecondSpace(name2);
-        // test3 = NameOps.printMethodCall(method, name3) + NameOps.indexOfSecondSpace(name3);
-        // System.out.println(test1); // -1
-        // System.out.println(test2); // -1
-        // System.out.println(test3); // 11
-        // System.out.println();
+        // testing indexOfSecondSpace()
+        System.out.println("== Testing indexOfSecondSpace() ==");
+        method = "indexOfSecondSpace";
+        test1 = NameOps.printMethodCall(method, name1) + NameOps.indexOfSecondSpace(name1);
+        test2 = NameOps.printMethodCall(method, name2) + NameOps.indexOfSecondSpace(name2);
+        test3 = NameOps.printMethodCall(method, name3) + NameOps.indexOfSecondSpace(name3);
+        System.out.println(test1); // -1
+        System.out.println(test2); // -1
+        System.out.println(test3); // 11
+        System.out.println();
 
-        // // testing findFirstName()
-        // System.out.println("== Testing findFirstName() ==");
-        // method = "findFirstName";
-        // test1 = NameOps.printMethodCall(method, name1) + NameOps.findFirstName(name1);
-        // test2 = NameOps.printMethodCall(method, name2) + NameOps.findFirstName(name2);
-        // test3 = NameOps.printMethodCall(method, name3) + NameOps.findFirstName(name3);
-        // System.out.println(test1); // Cher
-        // System.out.println(test2); // Elton
-        // System.out.println(test3); // Elvis
-        // System.out.println();
+        // testing findFirstName()
+        System.out.println("== Testing findFirstName() ==");
+        method = "findFirstName";
+        test1 = NameOps.printMethodCall(method, name1) + NameOps.findFirstName(name1);
+        test2 = NameOps.printMethodCall(method, name2) + NameOps.findFirstName(name2);
+        test3 = NameOps.printMethodCall(method, name3) + NameOps.findFirstName(name3);
+        System.out.println(test1); // Cher
+        System.out.println(test2); // Elton
+        System.out.println(test3); // Elvis
+        System.out.println();
 
-        // // testing findLastName()
-        // System.out.println("== Testing findLastName() ==");
-        // method = "findLastName";
-        // test1 = NameOps.printMethodCall(method, name1) + NameOps.findLastName(name1);
-        // test2 = NameOps.printMethodCall(method, name2) + NameOps.findLastName(name2);
-        // test3 = NameOps.printMethodCall(method, name3) + NameOps.findLastName(name3);
-        // System.out.println(test1); // 
-        // System.out.println(test2); // John
-        // System.out.println(test3); // Presley
-        // System.out.println();
+        // testing findLastName()
+        System.out.println("== Testing findLastName() ==");
+        method = "findLastName";
+        test1 = NameOps.printMethodCall(method, name1) + NameOps.findLastName(name1);
+        test2 = NameOps.printMethodCall(method, name2) + NameOps.findLastName(name2);
+        test3 = NameOps.printMethodCall(method, name3) + NameOps.findLastName(name3);
+        System.out.println(test1); // 
+        System.out.println(test2); // John
+        System.out.println(test3); // Presley
+        System.out.println();
 
-        // // testing findMiddleName()
-        // System.out.println("== Testing findMiddleName() ==");
-        // method = "findMiddleName";
-        // test1 = NameOps.printMethodCall(method, name1) + NameOps.findMiddleName(name1);
-        // test2 = NameOps.printMethodCall(method, name2) + NameOps.findMiddleName(name2);
-        // test3 = NameOps.printMethodCall(method, name3) + NameOps.findMiddleName(name3);
-        // System.out.println(test1); //
-        // System.out.println(test2); //
-        // System.out.println(test3); // Aaron
-        // System.out.println();
+        // testing findMiddleName()
+        System.out.println("== Testing findMiddleName() ==");
+        method = "findMiddleName";
+        test1 = NameOps.printMethodCall(method, name1) + NameOps.findMiddleName(name1);
+        test2 = NameOps.printMethodCall(method, name2) + NameOps.findMiddleName(name2);
+        test3 = NameOps.printMethodCall(method, name3) + NameOps.findMiddleName(name3);
+        System.out.println(test1); //
+        System.out.println(test2); //
+        System.out.println(test3); // Aaron
+        System.out.println();
 
-        // // testing generateLastFirstMidInitial()
-        // System.out.println("== Testing generateLastFirstMidInitial() ==");
-        // method = "generateLastFirstMidInitial";
-        // test1 = NameOps.printMethodCall(method, name1) + NameOps.generateLastFirstMidInitial(name1);
-        // test2 = NameOps.printMethodCall(method, name2) + NameOps.generateLastFirstMidInitial(name2);
-        // test3 = NameOps.printMethodCall(method, name3) + NameOps.generateLastFirstMidInitial(name3);
-        // System.out.println(test1); // Cher
-        // System.out.println(test2); // John, Elton
-        // System.out.println(test3); // Presley, Elvis A.
-        // System.out.println();
+        // testing generateLastFirstMidInitial()
+        System.out.println("== Testing generateLastFirstMidInitial() ==");
+        method = "generateLastFirstMidInitial";
+        test1 = NameOps.printMethodCall(method, name1) + NameOps.generateLastFirstMidInitial(name1);
+        test2 = NameOps.printMethodCall(method, name2) + NameOps.generateLastFirstMidInitial(name2);
+        test3 = NameOps.printMethodCall(method, name3) + NameOps.generateLastFirstMidInitial(name3);
+        System.out.println(test1); // Cher
+        System.out.println(test2); // John, Elton
+        System.out.println(test3); // Presley, Elvis A.
+        System.out.println();
     }
 
 }
